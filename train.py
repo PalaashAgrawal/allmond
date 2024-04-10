8fb4c01a10bb080f6491bac50341e8892bd49940
PalaashAgrawal <palaashagrawal29@gmail.com>
2024-04-11 00:29:48 +0800

    reorganization: minor

--- train.py
+++ train.py
@@ -12,68 +12,47 @@ from fastai.distributed import *
 from torch.utils.data import DataLoader
 
 
-import torch
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group, destroy_process_group
-from contextlib import nullcontext
 
 
-
-
-
-
-ddp = int(os.environ.get('RANK', -1))!=-1 #is this a dpp run?
-if ddp:
-    init_process_group(backend='nccl')
-    ddp_rank = int(os.environ['RANK'])
-    ddp_local_rank = int(os.environ['LOCAL_RANK'])
-    ddp_world_size = int(os.environ['WORLD_SIZE'])
-    
-    device = f'cuda:{ddp_local_rank}'
-    master_process = ddp_rank == 0
-    seed_offset = ddp_rank #each process get a different rank
-    
-
-device_type = 'cuda'
-dtype = 'bfloat16'
-ptdtype = {'float32': torch.float32, 'bfloat16': torch.bfloat16, 'float16': torch.float16}[dtype]
-torch.manual_seed(1337 + seed_offset)
-torch.backends.cuda.matmul.allow_tf32 = True # allow tf32 on matmul
-torch.backends.cudnn.allow_tf32 = True # allow tf32 on cudnn
-
-
-
-ctx = torch.amp.autocast(device_type = device_type, dtype = dtype) if ddp else nullcontext()
-
-
+os.environ['NCCL_P2P_DISABLE']='1'
+from accelerate import DistributedDataParallelKwargs
+from accelerate.utils import DistributedType
     
 # bs = 20 #distributed training
-bs=4
+bs=20
 block_size = 512
 valid_sampler_size = 1000 #how many samples to use for validation. This is only used to check if validation loss is better than best_valid_loss, so that a checkpoint can be saved. Karpathy uses 200 random points
 
-model = GPT(block_size=block_size).to(device)
-if ddp: model = DDP(model, device_ids=[ddp_local_rank])
 
 
-print('model done')
+
+model = GPT(block_size=block_size)
+
+
+
 tokenizer = TiktokenTokenizer(from_model = "gpt2")
 
 train_ds = OWTData(OpenWebTextConfig().default_cache_dir/'train.bin'    ,block_size=block_size, dtype=tokenizer._get_numpy_dtype())
 valid_ds = OWTData(OpenWebTextConfig().default_cache_dir/'val.bin'      ,block_size=block_size, dtype=tokenizer._get_numpy_dtype())
-                    
+
 train_dl = DataLoader(train_ds, batch_size=bs, pin_memory=True)
-
-valid_dl = DataLoader(valid_ds, batch_size=2*bs,
-                        sampler = RandomSubsetSampler(valid_ds, subset_size=valid_sampler_size),
-                        pin_memory = True
-                        )
-
+valid_dl = DataLoader(valid_ds, batch_size=2*bs,pin_memory = True,
+                        sampler = RandomSubsetSampler(valid_ds, subset_size=valid_sampler_size),)
 
 
 dls = DataLoaders(train_dl, valid_dl)
 dls.c = model.head.out_features
 
+
+# path = rank0_first(untar_data, URLs.WIKITEXT_TINY)
+# df_train = pd.read_csv(path/'train.csv', header=None)
+# df_valid = pd.read_csv(path/'test.csv', header=None)
+# df_all = pd.concat([df_train, df_valid])
+# splits = [list(range_of(df_train)), list(range(len(df_train), len(df_all)))]
+# tfms = [attrgetter("text"), Tokenizer.from_df(0), Numericalize()]
+# dsets = Datasets(df_all, [tfms], splits=splits, dl_type=LMDataLoader)
+# bs,sl = bs,block_size
+# dls = dsets.dataloaders(bs=bs, seq_len=sl)
 
 
     
@@ -82,41 +61,21 @@ check_and_save_model = save_model_checkpoints(dir = Path('checkpoints'),
                                                 checkpoint_name = 'gpt2', 
                                                 every_iters = 5000)
 
-
-class check_training_stages(Callback):
-    order=80
-    def before_fit(self):
-        print('before_fit')
-    def before_epoch(self):
-        print('before_epoch')
-    def before_train(self):
-        print('before_train')
-    def before_batch(self):
-        print('before_batch')
-    def after_batch(self):
-        print('after_batch')
-    def after_train(self):
-        print('after_train')
-    def after_epoch(self):
-        print('after_epoch')
-    def after_fit(self):
-        print('after_fit')
     
-    
-
-
 
 learn = customLearner(dls, 
                 model, 
                 loss_func=CrossEntropyLossFlat(), 
                 metrics=[accuracy, Perplexity()],
-                cbs = [check_and_save_model
-                        # , check_training_stages()
-                    ],
+                cbs = [check_and_save_model],
                 ).to_bf16()
 
 
 
+# from accelerate import Accelerator
+# accelerator = Accelerator(kwargs_handlers=[DistributedDataParallelKwargs(find_unused_parameters=True)])
+# learn.model = accelerator.prepare(nn.SyncBatchNorm.convert_sync_batchnorm(model))
+
+
 with learn.distrib_ctx():
-# with nullcontext():
     learn.fit_one_cycle(2, 1e-4)
