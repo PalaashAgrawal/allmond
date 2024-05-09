26c4509366e3db307328cd56d0f0f456deaada76
palaashagrawal <palaashagrawal29@gmail.com>
2024-05-09 20:48:35 +0800

    added FSDP support

--- train.py
+++ train.py
@@ -12,6 +12,8 @@ from fastai.callback.wandb import *
 import wandb
 
 
+print('is_fsdp?',os.environ.get("ACCELERATE_USE_FSDP", "false"))
+
 # os.environ['NCCL_P2P_LEVEL']='NVL'
 #If you see  see an error like this after waiting for a long time. 
 # RuntimeError: Exception occured in `DistributedTrainer` when calling event `before_fit`:  DDP expects same model across all ranks, but Rank 1 has 221 params, while rank 0 has inconsistent 0 params.
@@ -67,9 +69,18 @@ if log_wandb:
     cbs.append(WandbCallback())
     wandb.init(project=project, name = f"{id}_{str(model)}_{mode}")
 
+opt = torch.optim.AdamW(model.parameters(), lr=1e-5, betas=(0.9,0.95),
+                                 eps=1e-5, weight_decay=0.1, fused=False)
+
+def opt_func(params, **kwargs): return OptimWrapper(torch.optim.AdamW(params,betas=(0.9,0.95), eps=1e-5, weight_decay=0.1, fused=False, **kwargs)) #lr is passed in create_opt
+# print(isinstance(opt_func, OptimWrapper))
+
 
 learn = LLMLearner(dls, 
                 model, 
+                # opt_func = opt_func,
+                opt_func = partial(OptimWrapper, opt=torch.optim.AdamW),
+                
                 loss_func=CrossEntropyLossFlat(), 
                 metrics=[accuracy, Perplexity()],
                 cbs = cbs,
@@ -78,6 +89,6 @@ learn = LLMLearner(dls,
                 ).to_bf16()
 
 #check and load previous checkpoint. Doesnt make sense to do it within the callback, because all callbacks are initialized in the Learner before they are even called
-learn.check_and_load_learner(check_and_save_model.checkpoint_name, device = rank_distrib() if num_distrib() else None) #initialize each learner to respective device
+# learn.check_and_load_learner(check_and_save_model.checkpoint_name, device = rank_distrib() if num_distrib() else None) #initialize each learner to respective device
 
-with learn.distrib_ctx(): learn.fit_one_cycle(1, 1e-4)
+# with learn.distrib_ctx(): learn.fit_one_cycle(1, 1e-4)
