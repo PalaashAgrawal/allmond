23e51d2e4005ea22d0cef6278d6f277e64c81203
palaashagrawal <palaashagrawal29@gmail.com>
2024-05-09 21:25:23 +0800

    FSDP support

--- .docs/accelerate.md
+++ .docs/accelerate.md
@@ -6,7 +6,11 @@ First you need to set up configurations for distributed training. In your termin
 
 This will create a yaml configuration file in a default location (such as `~/.cache/huggingface/accelerate/default_config.yml`)
 
-Most defaults work well. Just set up  "distributed_type" (multi-gpu/multi-node?),  "num_machines",  "num_processes" (accross all the machines combined), and "machine_rank". An example of the yaml values that may be suitable for your config is shown below. This particular example is meant for multi-GPU training, within a single node.
+Most defaults work well. Just set up  "distributed_type" (multi-gpu/multi-node?),  "num_machines",  "num_processes" (accross all the machines combined), and "machine_rank". An example of the yaml values that may be suitable for your config is shown below. 
+
+
+### Example 1
+This particular example is meant for DDP based multi-GPU training, within a single node.
 
 ```
 {
@@ -25,4 +29,37 @@ Most defaults work well. Just set up  "distributed_type" (multi-gpu/multi-node?)
   "tpu_use_sudo": false,
   "use_cpu": false
 }
+```
+
+### Example 2
+This example shows FSDP based multi-GPU, single node training (with no CPU offloading)
+
+```
+compute_environment: LOCAL_MACHINE
+debug: false
+distributed_type: FSDP
+downcast_bf16: 'no'
+enable_cpu_affinity: false
+fsdp_config:
+  fsdp_auto_wrap_policy: SIZE_BASED_WRAP
+  fsdp_backward_prefetch: BACKWARD_PRE
+  fsdp_cpu_ram_efficient_loading: true
+  fsdp_forward_prefetch: false
+  fsdp_min_num_params: 100000000
+  fsdp_offload_params: false
+  fsdp_sharding_strategy: FULL_SHARD
+  fsdp_state_dict_type: SHARDED_STATE_DICT
+  fsdp_sync_module_states: true
+  fsdp_use_orig_params: true
+machine_rank: 0
+main_training_function: main
+mixed_precision: bf16
+num_machines: 1
+num_processes: 2
+rdzv_backend: static
+same_network: true
+tpu_env: []
+tpu_use_cluster: false
+tpu_use_sudo: false
+use_cpu: false
 ```
--- train.py
+++ train.py
@@ -11,9 +11,6 @@ from fastai.distributed import *
 from fastai.callback.wandb import *
 import wandb
 
-
-print('is_fsdp?',os.environ.get("ACCELERATE_USE_FSDP", "false"))
-
 # os.environ['NCCL_P2P_LEVEL']='NVL'
 #If you see  see an error like this after waiting for a long time. 
 # RuntimeError: Exception occured in `DistributedTrainer` when calling event `before_fit`:  DDP expects same model across all ranks, but Rank 1 has 221 params, while rank 0 has inconsistent 0 params.
@@ -28,7 +25,7 @@ dataset = "wikisimple"
 mode = 'scratch'
 # ________________________________________hyperparams and settings_________________________
 
-bs=20 #each GPU gets bs = 20, works good for a 24GB GPU
+bs=2 #each GPU gets bs = 20, works good for a 24GB GPU
 block_size = 512
 valid_sampler_size = 1000 #how many samples to use for validation. This is only used to check if validation loss is better than best_valid_loss, so that a checkpoint can be saved. Karpathy uses 200 random points
 validate_every = 1000 #1000 iterations, each iteration is bs*total_GPUs inputs
@@ -36,7 +33,7 @@ validate_every = 1000 #1000 iterations, each iteration is bs*total_GPUs inputs
 #________________________________________Model_____________________________________________
 
 
-model = GPT(block_size=block_size)
+model = GPT(block_size=block_size, n_layer = 12) 
 tokenizer = TiktokenTokenizer(from_model = "gpt2")
 
 #________________________________________data______________________________________________
@@ -69,18 +66,10 @@ if log_wandb:
     cbs.append(WandbCallback())
     wandb.init(project=project, name = f"{id}_{str(model)}_{mode}")
 
-opt = torch.optim.AdamW(model.parameters(), lr=1e-5, betas=(0.9,0.95),
-                                 eps=1e-5, weight_decay=0.1, fused=False)
-
-def opt_func(params, **kwargs): return OptimWrapper(torch.optim.AdamW(params,betas=(0.9,0.95), eps=1e-5, weight_decay=0.1, fused=False, **kwargs)) #lr is passed in create_opt
-# print(isinstance(opt_func, OptimWrapper))
-
 
 learn = LLMLearner(dls, 
                 model, 
-                # opt_func = opt_func,
                 opt_func = partial(OptimWrapper, opt=torch.optim.AdamW),
-                
                 loss_func=CrossEntropyLossFlat(), 
                 metrics=[accuracy, Perplexity()],
                 cbs = cbs,
@@ -89,6 +78,6 @@ learn = LLMLearner(dls,
                 ).to_bf16()
 
 #check and load previous checkpoint. Doesnt make sense to do it within the callback, because all callbacks are initialized in the Learner before they are even called
-# learn.check_and_load_learner(check_and_save_model.checkpoint_name, device = rank_distrib() if num_distrib() else None) #initialize each learner to respective device
+learn.check_and_load_learner(check_and_save_model.checkpoint_name, device = rank_distrib() if num_distrib() else None) #initialize each learner to respective device
 
-# with learn.distrib_ctx(): learn.fit_one_cycle(1, 1e-4)
+with learn.distrib_ctx(): learn.fit_one_cycle(1, 1e-4)
